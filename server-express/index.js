326432055e31336cebe610dd3191bfeb1a76969e
Niki <nikip1255@gmail.com>
2026-03-07 16:21:45 +0200

    NEW

--- server-express/index.js
+++ server-express/index.js
@@ -12,12 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://web-final-frontend-nx9p.onrender.com"
+  ],
+  credentials: true
+}));
 
 const ACCESS_COOKIE = "access_token";
 const REFRESH_COOKIE = "refresh_token";
